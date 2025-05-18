3e18f7a5386a0b42395d75d81e7557b876559372
Angelin D Fernandez <adfds05@gmail.com>
2025-05-18 07:50:14 -0500

    form and funtionality added

--- js/index.js
+++ js/index.js
@@ -30,3 +30,49 @@ for (let i = 0; i < skills.length; i++) {
 
     skillsList.appendChild(skill);
 }
+
+const botton = document.getElementById("color");
+  
+const chageColor = document.getElementById("Experience");
+
+
+let flag = false;
+botton.addEventListener("click", () => {
+    if (flag) {
+        chageColor.style.backgroundColor = "white";
+        flag = false;
+    } else {
+        chageColor.style.backgroundColor = "lightblue";
+        flag = true;
+    }
+});
+
+
+const respuestas = document.getElementById("formularioForm");
+
+respuestas.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const nombre = event.target.name.value;
+    const email = event.target.email.value;
+    const mensajeImput = event.target.message.value;
+
+    const sectionMensaje = document.getElementById("messages");
+    const lista = sectionMensaje.querySelector("ul");
+
+    const mensaje = document.createElement("li");
+    mensaje.innerHTML = `<strong>${nombre}</strong> <br> <a href="mailto:${email}">${email}</a> <br> <strong>${mensajeImput}</strong>`;
+
+    lista.appendChild(mensaje);
+
+    const buttonremove = document.createElement("button");
+    buttonremove.textContent = "Delete";
+    buttonremove.classList.add("button");
+    buttonremove.addEventListener("click", function () {
+        lista.removeChild(mensaje); 
+    });
+
+    mensaje.appendChild(buttonremove);
+    event.target.reset();
+})
+
